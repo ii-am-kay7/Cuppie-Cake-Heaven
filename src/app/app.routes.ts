4669d8554ea990aa0247da9d4492c04f93d76f7c
Kamie <kamielahheuvel0@gmail.com>
2025-09-15 15:08:15 +0200

    seperated html and css files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Menu } from './pages/menu/menu';
+import { Gallery } from './pages/gallery/gallery';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },      // default page
+  { path: 'about', component: About},
+  { path: 'menu', component: Menu },
+  { path: 'gallery', component: Gallery },
+  { path: 'contact', component: Contact },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+    routes: any
+ }
