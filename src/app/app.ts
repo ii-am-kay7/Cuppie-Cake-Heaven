11e0d88c6de226e1936cf5d1f7a44140efba739b
Kamie <kamielahheuvel0@gmail.com>
2025-09-25 11:12:09 +0200

    running code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { Gallery } from "./pages/gallery/gallery";
 import { Menu } from "./pages/menu/menu";
 import { Testimonials } from "./pages/testimonials/testimonials";
 import { Contact } from "./pages/contact/contact";
-import { Home } from "./pages/home/home";
+// import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [ Gallery, Menu, Testimonials, Contact, Home],
+  standalone: true,
+  imports: [ RouterOutlet, Gallery, Menu, Testimonials, Contact],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
